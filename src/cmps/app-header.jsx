9ab0db27d0abd74d4b00cb9cf10c6c80025953bc
Yarden Naor <naoryarden@gmail.com>
2023-01-19 14:01:54 +0200

    building filter

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -18,31 +18,65 @@ import { Routes, Route, useParams, useLocation } from 'react-router-dom';
 
 export function AppHeader() {
 
-    const [filterBy, setFilterBy] = useState({
-        maxPrice: Infinity,
-        type: '',
-        capacity: -Infinity,
-    })
+    const stay = useSelector((state) => state.stayModule.stay)
+    const location = useLocation();
+    const stayId = stay._id
+    const roomDetiles = `/rooms`
 
     const [isFilterShown, setIsFilterShown] = useState(false)
+    const [filterType, setFilterType] = useState('location')
 
-    function handleChange(ev) {
-        const field = ev.target.name
-        const value = ev.target.value
-        setFilterBy({ ...filterBy, [field]: value })
+    function onShowFilter(type) {
+        setIsFilterShown(true)
+        setFilterType(type)
     }
 
 
-    function onSaveFilter(ev) {
-        ev.preventDefault()
-        console.log('hi from save filter:')
-    }
+    if (location !== roomDetiles) {
 
-    function onShowFilter(by) {
-        console.log('by at ShowFilter:', by)
-    }
+        return (
+            <header className="app-header flex Rdetiles ">
+                <div className="logo-container"><img src={logo} alt="" /></div>
+                <div className="filter-container flex align-center">
+                    <button ><div className='btn-txt' onClick={() => onShowFilter('location')}>Anywhere</div></button><span className='line'>|</span>
+                    <button ><div className='btn-txt' onClick={() => onShowFilter('date')}>Anyweek</div></button><span className='line'>|</span>
+                    <button className='guests flex align-center' onClick={() => onShowFilter('capacity')}><div className='btn-txt'>Add guests</div>
+                        <div className="search-image img-container" onClick={(ev) => {
+                            ev.stopPropagation()
+                            onShowFilter('where')
+                        }} >
+                            <svg className='svg-white' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" >
+                                <g fill="none">
+                                    <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9" />
+                                </g>
+                            </svg>
+                        </div>
+                    </button>
+                </div>
+                <div className='flex'>
+                    <div className='header-buttons'>
+                        <Link className="host-link" to="/hosting">Switch to hosting</Link>
+                        <button><div className='i18n img-container'><img src={i18n} alt="" /></div></button>
+                    </div>
+                    <button className='user-nav flex'>
+                        <div className='img-container hamburger'>
+                            <img src={hamburger} alt="" />
+                        </div >
+                        <div className='img-container user'>
+                            <img className='user-img' src={userDfault} alt="" />
+                        </div>
+                    </button>
+                    {isFilterShown && <StayFilter onShowFilter={onShowFilter} filterType={filterType} />}
 
-    //     const user = useSelector(storeState => storeState.userModule.user)
+                </div>
+
+            </header>
+        )
+    } else {
+        return <h1>I am a room!</h1>
+    }
+}
+ //     const user = useSelector(storeState => storeState.userModule.user)
 
     //     async function onLogin(credentials) {
     //         try {
@@ -68,97 +102,6 @@ export function AppHeader() {
     //             showErrorMsg('Cannot logout')
     //         }
     //     }
-
-    const stay = useSelector((state) => state.stayModule.stay)
-
-    const stayId = stay._id
-
-    const location = useLocation();
-
-  
-
-    
-
-    const roomDetiles = `/rooms`
-
-
-    if (location !== roomDetiles) {
-        console.log('work!!')
-        return (
-            <header className="app-header flex Rdetiles ">
-                <div className="logo-container"><img src={logo} alt="" /></div>
-                <div className="filter-container flex align-center">
-                    <button ><div className='btn-txt'>Anywhere</div></button><span className='line'>|</span>
-                    <button ><div className='btn-txt'>Anyweek</div></button><span className='line'>|</span>
-                    <button className='guests flex align-center' ><div className='btn-txt'>Add guests</div>
-                        <div className="search-image img-container"><img src={lence} alt="" /></div>
-                    </button>
-                </div>
-                <div className='flex'>
-                    <div className='header-buttons'>
-                        <Link className="host-link" to="/hosting">Switch to hosting</Link>
-                        <button><div className='i18n img-container'><img src={i18n} alt="" /></div></button>
-                    </div>
-                    <button className='user-nav flex'>
-                        <div className='img-container hamburger'>
-                            <img src={hamburger} alt="" />
-                        </div >
-                        <div className='img-container user'>
-                            <img className='user-img' src={userDfault} alt="" />
-                            <StayFilter filterBy={filterBy} handleChange={handleChange} onSaveFilter={onSaveFilter}>
-                                <button className='loaction-filter'><div className='btn-txt'><div className='title'>where</div><div className='desc'>Search destination</div></div></button>
-
-                            </StayFilter>
-                        </div>
-                    </button>
-                </div>
-
-
-
-
-            </header>
-        )
-    }
-    return (
-        <header className="app-header flex ">
-            <div className="logo-container"><img src={logo} alt="" /></div>
-            <div className="filter-container flex align-center">
-                <button ><div className='btn-txt'>Anywhere</div></button><span className='line'>|</span>
-                <button ><div className='btn-txt'>Anyweek</div></button><span className='line'>|</span>
-                <button className='guests flex align-center' ><div className='btn-txt'>Add guests</div>
-                    <div className="search-image img-container">
-                        <svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation"
-                            focusable="false" ><g fill="none"></g>
-                        </svg>
-                    </div>
-                </button>
-            </div>
-            <div className='flex'>
-                <div className='header-buttons'>
-                    <Link className="host-link" to="/hosting">Switch to hosting</Link>
-                    <button><div className='i18n img-container'><img src={i18n} alt="" /></div></button>
-                </div>
-                <button className='user-nav flex'>
-                    <div className='img-container hamburger'>
-                        <img src={hamburger} alt="" />
-                    </div >
-                    <div className='img-container user'>
-                        <img className='user-img' src={userDfault} alt="" />
-                        <StayFilter filterBy={filterBy} handleChange={handleChange} onSaveFilter={onSaveFilter} onShowFilter={onShowFilter} />
-                    </div>
-                </button>
-            </div>
-
-
-
-
-        </header>
-    )
-}
-
-
-
-
 
 // <nav>
 //                 {routes.map(route => <NavLink key={route.path} to={route.path}>{route.label}</NavLink>)}
--- src/cmps/stay-filter.jsx
+++ src/cmps/stay-filter.jsx
@@ -1,33 +1,92 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
+import { LocationFilter } from '../cmps/location-stay-filter'
+import { DateFilter } from '../cmps/date-stay-filter'
+import { CapacityFilter } from '../cmps/capacity-stay-filter'
 // import { TextField, Select, MenuItem, Checkbox, FormControlLabel, FormControl, InputLabel } from '@material-ui/core'
 
-export function StayFilter({ handleChange, filterBy,onShowFilter }) {
+export function StayFilter({ filterType }) {
+    const [filterToShow, setFilterToShow] = useState(null)
+    const [filterBy, setFilterBy] = useState({
+        maxPrice: Infinity,
+        type: '',
+        capacity: -Infinity,
+    })
 
-    console.log('hi from filter:')
+
+    // useEffect(() => {
+    //   console.log('filterType:',filterType)
+    //    const filter= showFilter(filterType)
+    //     setFilter(filter)
+    // }, [])
+
+    useEffect(() => {
+        console.log('filterType: at use efferct', filterType)
+        showFilter(filterType)
+    }, [])
+
+    function handleChange(ev) {
+        const field = ev.target.name
+        const value = ev.target.value
+        setFilterBy({ ...filterBy, [field]: value })
+    }
+
+    function onSaveFilter(ev) {
+        ev.preventDefault()
+        console.log('hi from save filter:')
+    }
+
+    function showFilter(type) {
+        console.log('type at show filter', type)
+        switch (type) {
+            case 'location':
+                setFilterToShow(<LocationFilter />)
+                break
+            case 'date':
+                setFilterToShow(<DateFilter />)
+                break
+            case 'capacity':
+                setFilterToShow(<CapacityFilter />)
+                break
+            default:
+                setFilterToShow(<LocationFilter />)
+        }
+    }
 
     return (
-        <div className="filter-container">
-            <button className='loaction-btn' onClick={() => onShowFilter('Where')}>
+        <div className="filter-btn-container">
+            <button className='loaction-btn' onClick={() => showFilter('location')}>
                 <div className='btn-txt'><div className='title' >where</div>
                     <div className='desc'>Search destination</div>
                 </div>
             </button>
-            <button className='checkIn-btn' onClick={() => onShowFilter('checkIn')}>
+            <button className='checkIn-btn' onClick={() => showFilter('date')}>
                 <div className='btn-txt'><div className='title' >Check in</div>
                     <div className='desc'>Add dates</div>
                 </div>
             </button>
-            <button className='checkOut-btn' onClick={() => onShowFilter('checkOut')}>
+            <button className='checkOut-btn' onClick={() => showFilter('date')}>
                 <div className='btn-txt'><div className='title'>Check out</div>
                     <div className='desc'>Add dates</div>
                 </div>
             </button>
-            <button className='who-btn' onClick={() => onShowFilter('who')}>
+            <button className='who-btn' onClick={() => showFilter('capacity')}>
                 <div className='btn-txt'>
                     <div className='title'>Who</div>
                     <div className='desc'>Add guests</div>
                 </div>
             </button>
+            <form onSubmit={onSaveFilter}>
+                <button className="search-btn" > <div className="search-image img-container">
+                    <span> Search</span> <svg className='svg-white' viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" >
+                        <g fill="none">
+                            <path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9" />
+                        </g>
+                    </svg>
+                </div>
+                </button>
+            </form>
+            {filterToShow}
         </div>
     )
 }
