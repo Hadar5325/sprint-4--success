9f509b82cf85003dcb00b6978dc40a509fe62045
Aviv Liberman <liberman809@gmail.com>
2023-01-20 13:50:58 +0200

    boxBooking on rooms pages

--- src/cmps/rooms/booking.jsx
+++ src/cmps/rooms/booking.jsx
@@ -1,12 +1,19 @@
-
+import React, { useEffect, useState } from 'react'
+import {GuestsModal} from './guestsModal.jsx'
 
 export function Booking(){
+    const [gModal,setgModal] = useState(false)
+
+
     return <section className="booking">
         Booking
         <div className="bookingBox">
             <div className="boxContantTop">
-                
+                <button className="checkInBtn bookingBtn" onClick={() => setgModal(true)}></button>
+                <hr></hr>
+                <button className="guests bookingBtn" onClick={() => setgModal(true)}></button>
             </div>
+            {gModal && <GuestsModal/>}
         </div>
     </section>
 }
--- src/cmps/rooms/galeryLeft.jsx
+++ src/cmps/rooms/galeryLeft.jsx
@@ -4,9 +4,6 @@ export function GaleryLeft({room}){
 
     const url = room['imgUrls'][0]
     
-    return <section>
-        {
-        <img className="leftGalery" src={url}/>
-        }
-    </section>
+    return <img className='galeryLeft' src={url}/>
+       
 }
